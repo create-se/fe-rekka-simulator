3a80bbc483c9e55e462e6c158e8fe2fd41d914d2
hashi654 <clsy.engineering@gmail.com>
2026-01-13 16:19:43 +0900

    神将器に竜特攻を追加（ソール・カティ、デュランダル、アルマーズ、フォルブレイズ）

--- data/weapons.js
+++ data/weapons.js
@@ -21,8 +21,8 @@ const WEAPONS = {
         { id: "light_brand", name: "ライトブランド", type: "sword", might: 9, weight: 9, hit: 70, crit: 0, range: [1, 2], rank: "C", magic: true },
         { id: "rune_sword", name: "ルーンソード", type: "sword", might: 12, weight: 11, hit: 65, crit: 0, range: [1, 2], rank: "A", magic: true },
         { id: "mani_katti", name: "マーニ・カティ", type: "sword", might: 8, weight: 3, hit: 80, crit: 20, range: [1], rank: "Prf", effective: ["armor", "cavalry"] },
-        { id: "sol_katti", name: "ソール・カティ", type: "sword", might: 12, weight: 14, hit: 95, crit: 25, range: [1], rank: "Prf" },
-        { id: "durandal", name: "デュランダル", type: "sword", might: 17, weight: 16, hit: 90, crit: 0, range: [1], rank: "Prf", bonusStr: 5 },
+        { id: "sol_katti", name: "ソール・カティ", type: "sword", might: 12, weight: 14, hit: 95, crit: 25, range: [1], rank: "Prf", effective: ["dragon"] },
+        { id: "durandal", name: "デュランダル", type: "sword", might: 17, weight: 16, hit: 90, crit: 0, range: [1], rank: "Prf", bonusStr: 5, effective: ["dragon"] },
         { id: "regal_blade", name: "レイピア", type: "sword", might: 7, weight: 5, hit: 95, crit: 10, range: [1], rank: "Prf", effective: ["armor", "cavalry"] },
     ],
 
@@ -56,7 +56,7 @@ const WEAPONS = {
         { id: "hammer", name: "ハンマー", type: "axe", might: 10, weight: 15, hit: 55, crit: 0, range: [1], rank: "D", effective: ["armor"] },
         { id: "devil_axe", name: "デビルアクス", type: "axe", might: 18, weight: 18, hit: 55, crit: 0, range: [1], rank: "E", devil: true },
         { id: "basilikos", name: "バシリコス", type: "axe", might: 22, weight: 13, hit: 75, crit: 0, range: [1], rank: "S" },
-        { id: "armads", name: "アルマーズ", type: "axe", might: 18, weight: 18, hit: 85, crit: 0, range: [1], rank: "Prf", bonusDef: 5 },
+        { id: "armads", name: "アルマーズ", type: "axe", might: 18, weight: 18, hit: 85, crit: 0, range: [1], rank: "Prf", bonusDef: 5, effective: ["dragon"] },
         { id: "wolf_beil", name: "ヴォルフバイル", type: "axe", might: 10, weight: 10, hit: 75, crit: 5, range: [1], rank: "Prf", effective: ["armor", "cavalry"] },
     ],
 
@@ -80,7 +80,7 @@ const WEAPONS = {
         { id: "bolting", name: "ボルティング", type: "anima", might: 12, weight: 20, hit: 60, crit: 0, range: [3, 10], rank: "A" },
         { id: "fimbulvetr", name: "フィンブル", type: "anima", might: 13, weight: 12, hit: 80, crit: 0, range: [1, 2], rank: "A" },
         { id: "excalibur", name: "エクスカリバー", type: "anima", might: 18, weight: 13, hit: 90, crit: 10, range: [1, 2], rank: "S", effective: ["flying"] },
-        { id: "forblaze", name: "フォルブレイズ", type: "anima", might: 14, weight: 11, hit: 85, crit: 5, range: [1, 2], rank: "Prf", bonusRes: 5 },
+        { id: "forblaze", name: "フォルブレイズ", type: "anima", might: 14, weight: 11, hit: 85, crit: 5, range: [1, 2], rank: "Prf", bonusRes: 5, effective: ["dragon"] },
     ],
 
     // ===== 光魔法 =====
